761a1725bd490eaf7c7aa91fc6c67e79255a8e29
Sakshi Hedke <sakshihedke6@gmail.com>
2025-07-16 10:08:18 +0530

    Removed hardcoded API key and used environment variable

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBIBsXtehNfES1mFBB8uihSW_5TOQWbZho",
-  authDomain: "prepedge-da86c.firebaseapp.com",
-  projectId: "prepedge-da86c",
-  storageBucket: "prepedge-da86c.firebasestorage.app",
-  messagingSenderId: "367491266663",
-  appId: "1:367491266663:web:6ca7c8e49308ace5fafcc2",
-  measurementId: "G-T4438ZNCRD"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
